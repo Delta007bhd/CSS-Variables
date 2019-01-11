d875b644b2989dce6dafe65640d8b7413e892840
Delta007bhd <46333260+Delta007bhd@users.noreply.github.com>
2019-01-11 01:45:54 +0100

    Improved responsiveness, design and added comments

--- swatches.js
+++ swatches.js
@@ -1,8 +1,18 @@
 var swatches = document.querySelectorAll('.swatches span');
 var root = document.querySelector(':root');
-
+//arrow function
 swatches.forEach((swatch) => {
     swatch.addEventListener('click', (e) => {
         root.style.setProperty('--theme-color', e.target.style.background);
     });
 });
+
+//
+// var swatches = document.querySelectorAll('.swatches span');
+// var root = document.querySelector(':root');
+//
+// swatches.forEach(fucntion(swatch){
+//     swatch.addEventListener('click', function(e){
+//         root.style.setProperty('--theme-color', e.target.style.background);
+//     });
+// });
